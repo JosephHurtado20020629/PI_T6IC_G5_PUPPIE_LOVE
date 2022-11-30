1d7aafdcaae802eee636082b3d2ee0c05ae83cb4
Joseph Hurtado <joseph.hurtado.magan@gmail.com>
2022-11-30 18:59:47 -0500

    fix

--- src/main/java/com/puppie/puppie_love/Controllers/HomeController.java
+++ src/main/java/com/puppie/puppie_love/Controllers/HomeController.java
@@ -2,12 +2,15 @@ package com.puppie.puppie_love.Controllers;
 
 import com.puppie.puppie_love.Models.DetallePedido;
 import com.puppie.puppie_love.Models.Producto;
+import com.puppie.puppie_love.Models.Usuario;
 import com.puppie.puppie_love.Repositorys.IDetallePedidosRepository;
 import com.puppie.puppie_love.Repositorys.IPedidosRepository;
 import com.puppie.puppie_love.Repositorys.IProductoRepository;
 import com.puppie.puppie_love.Repositorys.IUsuarioRepository;
 import com.puppie.puppie_love.Utils.ConstantsPage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping(value = "home")
@@ -35,6 +39,16 @@ public class HomeController {
 
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
     public String index(Model model, HttpServletRequest request) {
+
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Usuario usuario =null;
+        if(Objects.isNull(request.getSession().getAttribute("cliente"))) {
+           usuario= usuarioRepository.findUsuarioByUsername(userDetails.getUsername());
+            request.getSession().setAttribute("cliente", usuario);
+            request.getSession().setAttribute("carrito", new ArrayList<DetallePedido>());
+        }
+
+
         List<DetallePedido> listaPedidos = detallpedidosrepo.masVendidos();
         List<Producto> listaProductos = new ArrayList<>();
 
